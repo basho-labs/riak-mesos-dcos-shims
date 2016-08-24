74399340e1123a1d79cb62658404bb67d009b93b
sanmiguel <michael.coles@gmail.com>
2016-08-24 16:56:05 +0200

    Fix license classifier

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
         'Intended Audience :: Information Technology',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: TODO: License',
+        'License :: OSI Approved :: Apache Software License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
